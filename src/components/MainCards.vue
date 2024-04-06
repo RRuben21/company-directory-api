bff3548d53d55945c104e0f71dab61fd78709f54
RRuben21 <rubenrosa.76@gmail.com>
2024-04-06 18:36:38 -0500

    connect to api is working

--- src/components/MainCards.vue
+++ src/components/MainCards.vue
@@ -1,11 +1,15 @@
 <script setup>
 import MaincardSingle from './MaincardSingle.vue/';
+import useAPI from '@/composables/useAPI';
+
+const {employees} = useAPI()
+
 </script>
 
 
 <template>
 <div class="grid grid-cols-5 gap-10 p-10">
-    <MaincardSingle v-for = "card in 20" :key="card"></MaincardSingle>
+    <MaincardSingle v-for = "employee in employees" :key="employee._id" :employee="employee"></MaincardSingle>
 </div>
 
 
